b76352c7717b340e33a8f0c3e1e15bd5f2d31971
liuhongwei <liuhongwei_wybj@163.com>
2023-07-11 22:29:18 +0800

    分包优化

---
+++ subPackages/quanzi/mylist/mylist.vue
@@ -0,0 +1,82 @@
+<template>
+	<view class="container">
+		<unicloud-db
+			ref="udb"
+			v-slot:default="{ data, pagination, loading, hasMore, error }"
+			collection="quanzi_article"
+			:where="{ user_id: db.getCloudEnv('$cloudEnv_uid'), delState: false }"
+			field="_id,publish_date,user_id,title"
+			orderby="publish_date desc"
+			:distinct="true"
+		>
+			<view v-if="error">{{ error.message }}</view>
+			<view v-else-if="data">
+				<uni-list>
+					<uni-list-item v-for="(item, index) in data" :key="index" showArrow :clickable="true" @click="handleItemClick(item)">
+						<template v-slot:body>
+							<text>
+								<!-- 此处默认显示为_id，请根据需要自行修改为其他字段 -->
+								<!-- 如果使用了联表查询，请参考生成的 admin 项目中 list.vue 页面 -->
+								{{ item.title }}
+							</text>
+						</template>
+					</uni-list-item>
+				</uni-list>
+			</view>
+			<uni-load-more :status="loading ? 'loading' : hasMore ? 'more' : 'noMore'"></uni-load-more>
+		</unicloud-db>
+		<uni-fab ref="fab" horizontal="right" vertical="bottom" :pop-menu="false" @fabClick="fabClick" />
+	</view>
+</template>
+
+<script>
+const db = uniCloud.database();
+export default {
+	data() {
+		return {
+			loadMore: {
+				contentdown: '',
+				contentrefresh: '',
+				contentnomore: ''
+			},
+			db
+		};
+	},
+	onPullDownRefresh() {
+		this.$refs.udb.loadData(
+			{
+				clear: true
+			},
+			() => {
+				uni.stopPullDownRefresh();
+			}
+		);
+	},
+	onReachBottom() {
+		this.$refs.udb.loadMore();
+	},
+	methods: {
+		handleItemClick(item) {
+			uni.navigateTo({
+				url: '/pages/detail/detail?id=' + item._id
+			});
+		},
+		fabClick() {
+			// 打开新增页面
+			uni.navigateTo({
+				url: './add',
+				events: {
+					// 监听新增数据成功后, 刷新当前页面数据
+					refreshData: () => {
+						this.$refs.udb.loadData({
+							clear: true
+						});
+					}
+				}
+			});
+		}
+	}
+};
+</script>
+
+<style></style>
